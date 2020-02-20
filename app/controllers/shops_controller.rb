6d027e606942c43dce757fcc8061bfe85a4c4de8
JasonP77 <llvslr77@gmail.com>
2020-02-20 17:28:08 -0500

    push

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -11,4 +11,39 @@ class ShopsController < Sinatra::Base
         erb :index
     end
 
+    get '/shops/new' do
+        erb :new
+    end
+
+    post '/shops' do
+        @name = params[:name]
+        @location = params[:location]
+        @shop = Shop.find_or_create_by(name: @name, location: @location)
+        redirect "/shops/#{@shop.id}"
+    end
+
+    get '/shops/:id' do
+        @shop = Shop.find(params[:id])
+        erb :show
+    end
+
+    delete '/shops/:id' do
+        @shop = Shop.find(params[:id])
+        @shop.destroy
+        redirect '/shops'
+    end
+
+    get '/shops/:id/edit' do
+        @shop = Shop.find(params[:id])
+        erb :edit
+    end
+
+    post '/shops/:id' do
+        @shop = Shop.find(params[:id])
+        @name = params[:name]
+        @location = params[:location]
+        @shop.update(name: @name, location: @location)
+        redirect "/shops/#{@shop.id}"
+    end
+
 end
