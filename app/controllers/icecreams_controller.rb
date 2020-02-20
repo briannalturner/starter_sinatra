3f32293f41dff1393a79d5d18e1b9b1062fc5bc8
JasonP77 <llvslr77@gmail.com>
2020-02-20 16:16:24 -0500

    halfway

--- app/controllers/icecreams_controller.rb
+++ app/controllers/icecreams_controller.rb
@@ -5,6 +5,7 @@ require_relative '../../config/environment'
 class IcecreamsController < Sinatra::Base
 
     set :views, 'app/views/icecream'
+    set :method_override, true
 
     get '/icecreams' do
         @icecreams = Icecream.all
@@ -12,6 +13,27 @@ class IcecreamsController < Sinatra::Base
         erb :index
     end
 
+    get '/icecreams/new' do
+        erb :new
+    end
+
+    post '/icecreams' do
+        @flavor = params[:flavor]
+        @brand = params[:brand]
+        @price = params[:price]
+        @icecream = Icecream.find_or_create_by(flavor: @flavor, brand: @brand, price: @price)
+        redirect "/icecreams/#{@icecream.id}"
+    end
+    get '/icecreams/:id' do
+        @icecream = Icecream.find(params[:id])
+        erb :show
+    end
+
+    delete '/icecreams/:id' do
+        @icecream = Icecream.find(params[:id])
+        @icecream.destroy
+        redirect "/icecreams"
+    end
 
 
 end
